bdcc81ce3963d2c746af4ac3078131acd0cf2d68
maludatech <nwitevictor7@gmail.com>
2025-11-25 02:45:33 +0100

    update: done with Dbh.php

--- src/Classes/Dbh.php
+++ src/Classes/Dbh.php
@@ -2,4 +2,22 @@
 
 declare(strict_types=1);
 
-class Dbh {};
+class Dbh
+{
+    private $host = 'localhost';
+    private $dbname = 'core-auth';
+    private  $username = 'root';
+    private  $password = '';
+
+    protected function connect()
+    {
+        try {
+            $dsn = 'mysql:host=' . $this->host . ';dbname=' . $this->dbname;
+            $pdo = new PDO($dsn, $this->username, $this->password);
+            $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+            return $pdo;
+        } catch (PDOException $e) {
+            die("Database connection failed: " . $e->getMessage());
+        }
+    }
+};

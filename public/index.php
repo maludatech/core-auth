6a307011d36d4b3ab4df4a603a45b5b4a82c9234
maludatech <nwitevictor7@gmail.com>
2025-11-25 02:30:03 +0100

    update: intial commit

---
+++ public/index.php
@@ -0,0 +1,20 @@
+<?php
+
+declare(strict_types=1);
+?>
+
+
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Core Auth</title>
+    <script src="https://cdn.tailwindcss.com"></script>
+</head>
+
+<body>
+
+</body>
+
+</html>
